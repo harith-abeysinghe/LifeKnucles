5e76a34e4ed0ac26d8670d4b8cc3ecfbba887ad2
Dasun Theekshana <dasun.theekshana.git@gmail.com>
2024-08-19 19:44:46 +0530

    remove unwanted files, add fonts, nativewind and tailwindcss

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="w-full flex justify-center items-center h-full px-4">
+      <Text className="text-3xl font-pblack"> textInComponent </Text>
+      <Text>Header Edit</Text>
+    </View>
+  );
+}
